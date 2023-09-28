2343b0c6e889a18d93254276849af623bb1704d2
tornike khvadagiani <tornikexvadagiani1@gmail.com>
2023-09-28 23:26:02 +0400

    render user data in html

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  styleUrls: ['./user.component.scss'],
 })
 export class UserComponent implements OnInit {
+  @Input() user!: User;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.scss']
+  styleUrls: ['./users-list.component.scss'],
 })
 export class UsersListComponent implements OnInit {
+  users: User[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private userService: UsersService) {
+    this.userService.getAllUsers().then((users) => {
+      this.users = users;
+    });
   }
 
+  ngOnInit(): void {}
 }
