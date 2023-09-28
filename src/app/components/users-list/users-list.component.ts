864cd92f954d0ed310dc04b3a9fe19f77fa1ffdd
tornike khvadagiani <tornikexvadagiani1@gmail.com>
2023-09-29 00:12:27 +0400

    infinite scroll added

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { UsersService } from 'src/app/services/users.service';
 
@@ -8,11 +8,45 @@ import { UsersService } from 'src/app/services/users.service';
   styleUrls: ['./users-list.component.scss'],
 })
 export class UsersListComponent implements OnInit {
+  usersData: User[] = [];
+
   users: User[] = [];
 
+  isLoading = false;
+
+  pageSize = 1;
+
+  pageCount = 30;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    if (
+      window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+      !this.isLoading &&
+      this.users.length !== this.usersData.length
+    ) {
+      this.isLoading = true;
+
+      setTimeout(() => {
+        this.users = [
+          ...this.users,
+          ...this.usersData.slice(
+            this.pageSize * this.pageCount,
+            this.pageSize * this.pageCount + this.pageCount
+          ),
+        ];
+        this.pageSize += 1;
+        this.isLoading = false;
+      }, 1000);
+    }
+  }
+
   constructor(private userService: UsersService) {
+    this.isLoading = true;
     this.userService.getAllUsers().then((users) => {
-      this.users = users;
+      this.usersData = users;
+      this.users = this.usersData.slice(0, this.pageCount);
+      this.isLoading = false;
     });
   }
 
